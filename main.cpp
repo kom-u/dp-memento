6e9045f18773702209ad0879d7a8bd021e89f798
Atqa Munzir <atqamz@gmail.com>
2023-05-24 09:51:34 +0700

    adding example for tictactoe

--- main.cpp
+++ main.cpp
@@ -1,24 +1,69 @@
 #include "lib.hpp"
 
+#include <iostream>
+
+void PrintBoard(std::string _boardString)
+{
+    // convert board to char 2d array
+    char boardArray[3][3];
+    int boardStringIndex = 0;
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            boardArray[i][j] = _boardString[boardStringIndex];
+            boardStringIndex++;
+        }
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        std::cout << "\t ";
+        for (int j = 0; j < 3; j++)
+        {
+            if (j != 2)
+                std::cout << boardArray[i][j] << " | ";
+            else
+                std::cout << boardArray[i][j];
+        }
+        std::cout << " ";
+
+        if (i != 2)
+            std::cout << std::endl
+                      << "\t---|---|---"
+                      << std::endl;
+    }
+
+    for (int i = 0; i < 9; i++)
+    {
+    }
+
+    std::cout << std::endl;
+}
+
 int main()
 {
-    Originator originator;
+    Originator board;
     Caretaker caretaker;
 
-    // Mengatur keadaan internal originator
-    originator.SetState("Keadaan Awal");
-    std::cout << "Keadaan Awal: " << originator.GetState() << std::endl;
+    std::cout << "State 1:\n";
+    board.SetState("X   X   O");
+    PrintBoard(board.GetState());
+    caretaker.SaveMemento(board.CreateMemento());
 
-    // Simpan keadaan menggunakan caretaker
-    caretaker.SaveMemento(originator.CreateMemento());
+    std::cout << "\n\n";
 
-    // Mengubah keadaan internal originator
-    originator.SetState("Keadaan Baru");
-    std::cout << "Keadaan Baru: " << originator.GetState() << std::endl;
+    std::cout << "State 2:\n";
+    board.SetState("X   X  XO");
+    PrintBoard(board.GetState());
 
     // Mengembalikan keadaan sebelumnya menggunakan caretaker
-    originator.RestoreMemento(caretaker.GetMemento());
-    std::cout << "Keadaan Setelah Restore: " << originator.GetState() << std::endl;
+    board.RestoreMemento(caretaker.GetMemento());
+
+    std::cout << "\n\n";
+    std::cout << "State 1 (Restored):\n";
+    PrintBoard(board.GetState());
 
     return 0;
 }
